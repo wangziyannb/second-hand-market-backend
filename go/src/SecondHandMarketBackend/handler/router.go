39882820df287a6f36960d1500f32b0e94e8d5e6
xyzhao009 <79874305+xyzhao009@users.noreply.github.com>
2022-07-01 11:36:52 -0700

    delete header in router.go

--- go/src/SecondHandMarketBackend/handler/router.go
+++ go/src/SecondHandMarketBackend/handler/router.go
@@ -2,7 +2,7 @@
  * @Author: xyzhao009 79874305+xyzhao009@users.noreply.github.com
  * @Date: 2022-06-30 13:16:21
  * @LastEditors: xyzhao009 79874305+xyzhao009@users.noreply.github.com
- * @LastEditTime: 2022-06-30 18:15:09
+ * @LastEditTime: 2022-07-01 11:36:32
  * @FilePath: /second-hand-market-backend-3/go/src/SecondHandMarketBackend/handler/router.go
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
